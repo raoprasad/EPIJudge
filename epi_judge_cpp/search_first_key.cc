147e82798c9f56966cceabef4f0b309ba28c998f
prasad <prasad.rao@gmail.com>
2019-03-08 06:27:51 +0530

    Search a sorted array for first occurance of k::Done

--- epi_judge_cpp/search_first_key.cc
+++ epi_judge_cpp/search_first_key.cc
@@ -2,9 +2,55 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+
+//Test case 1:
+//[],10
+
+//Test case 2:
+//[10, 10, 10], 10
+
+//Test case 3:
+//[1,2,2,3,4,4,4,5,6,7,7,7,7,8], 7
+//int SearchFirstOfK(const vector<int>& A, int k) {
+//	int begin = 0, end = A.size();
+//	while (begin < end) {
+//		int mid = (end + begin) >> 1;
+//		if (A[mid] == k) {
+//			int i = mid;
+//			while (begin < mid) {
+//				i = (begin + mid) >> 1;
+//				if (A[i] == k) {
+//					mid = i;
+//				}
+//				else if (A[i] < k) {
+//					begin = i + 1;
+//				}
+//			}
+//			return mid;
+//		}
+//		else if (A[mid] > k) {
+//			end = mid;
+//		}
+//		else begin = mid + 1;
+//	}
+//	return -1;
+//}
+
 int SearchFirstOfK(const vector<int>& A, int k) {
-  // TODO - you fill in here.
-  return 0;
+	int begin = 0, end = A.size();
+	int index = -1;
+	while (begin < end) {
+		int mid = (begin + end) >> 1;
+		if (A[mid] == k) {
+			index = mid;
+			end = mid;
+		}
+		else if (A[mid] > k) {
+			end = mid;
+		}
+		else begin = mid + 1;
+	}
+	return index;
 }
 
 int main(int argc, char* argv[]) {
