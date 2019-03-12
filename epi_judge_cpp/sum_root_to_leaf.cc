dc9d04b39345b7d33dcd0f29f3b5044b7e742205
prasad <prasad.rao@gmail.com>
2019-03-12 06:37:42 +0530

    Sum the root-to-leaf paths in a binary tree: Done

--- epi_judge_cpp/sum_root_to_leaf.cc
+++ epi_judge_cpp/sum_root_to_leaf.cc
@@ -1,9 +1,23 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 
+void Sum(const unique_ptr<BinaryTreeNode<int>>& tree, int num, int& sum) {
+	if (tree == nullptr) return;
+
+	num << 1;
+	num |= tree->data;
+
+	if (tree->left == nullptr && tree->right == nullptr) {
+		sum += num;
+	}
+	Sum(tree->left, num, sum);
+	Sum(tree->right, num, sum);
+}
+
 int SumRootToLeaf(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return 0;
+	int sum = 0;
+	Sum(tree, 0, sum);
+	return sum;
 }
 
 int main(int argc, char* argv[]) {
