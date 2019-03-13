74db5495436a5ad6af21006b3a6f474c4057f4fa
prasad <prasad.rao@gmail.com>
2019-03-13 06:25:40 +0530

    Implement an inorder traversal without recursion: Done...Needs refactoring. Also not happy with the visited flag.

--- epi_judge_cpp/tree_inorder.cc
+++ epi_judge_cpp/tree_inorder.cc
@@ -2,12 +2,30 @@
 #include <vector>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
-using std::unique_ptr;
-using std::vector;
+#include <stack>
+using namespace std;
 
 vector<int> InorderTraversal(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+	stack<pair<BinaryTreeNode<int>*, bool>> stk;
+	vector<int> res;
+	if (tree == nullptr) return res;
+
+	stk.push({ tree.get(), true });
+	while (!stk.empty()) {
+		auto cont = ref(stk.top());
+		while (cont.get().first->left != nullptr && cont.get().second) {
+			cont.get().second = false;
+			auto next = make_pair(cont.get().first->left.get(), true );
+			stk.push(next);
+			cont = ref(stk.top());
+		}
+		
+		res.emplace_back(cont.get().first->data);
+		stk.pop();
+
+		if (cont.get().first->right != nullptr) stk.push({ cont.get().first->right.get(), true });
+	}
+	return res;
 }
 
 int main(int argc, char* argv[]) {
