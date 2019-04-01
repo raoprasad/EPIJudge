cb47f3c7d3e840b364da9ccd7b30082e0d30ac49
prasad <prasad.rao@gmail.com>
2019-04-02 05:26:12 +0530

    The 3-sum problem: Done

--- epi_judge_cpp/three_sum.cc
+++ epi_judge_cpp/three_sum.cc
@@ -2,9 +2,23 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+bool hasTwoSum(vector<int> A, int t) {
+  int begin = 0, end = A.size()-1;
+  while(begin <= end){
+    int sum = A[begin] + A[end];
+    if(sum == t) return true;
+    else if(sum < t) begin++;
+    else end--;
+  }
+  return false;
+}
+
 bool HasThreeSum(vector<int> A, int t) {
-  // TODO - you fill in here.
-  return true;
+  sort(A.begin(), A.end());
+  for(int a : A){
+    if(hasTwoSum(A, t-a)) return true;
+  }
+  return false;
 }
 
 int main(int argc, char* argv[]) {
