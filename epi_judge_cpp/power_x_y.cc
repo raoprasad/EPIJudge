8c0a1ff741f55e9bd4a63447dc7fcadf38797847
prasad <prasad.rao@gmail.com>
2019-04-06 06:03:35 +0530

    Compute x^y: With O(logy) space. Need to do better

--- epi_judge_cpp/power_x_y.cc
+++ epi_judge_cpp/power_x_y.cc
@@ -1,7 +1,26 @@
 #include "test_framework/generic_test.h"
+#include <map>
+using namespace std;
+
 double Power(double x, int y) {
-  // TODO - you fill in here.
-  return 0.0;
+  cout << "Input num = " << x << " and input power = " << y << endl;
+  bool neg = (y < 0);
+  y = abs(y);
+  double ans = 1.0;
+  map<int, double> powers({{1, x}});
+  for(int i = 2; i <= y; i *= 2){
+    powers[i] = powers[i/2] * powers[i/2];
+  }
+
+  for (map<int, double>::reverse_iterator iter  = powers.rbegin();
+       iter != powers.rend(); ++iter ) {
+    if(y >= (*iter).first){
+      ans *= (*iter).second;
+      y -= (*iter).first;
+    }
+  }
+
+  return (neg)? 1/ans:ans;
 }
 
 int main(int argc, char* argv[]) {
