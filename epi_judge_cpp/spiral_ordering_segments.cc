516b9573258c8906ce7dfa787f4a86c3a6644b91
prasad <prasad.rao@gmail.com>
2019-04-06 07:55:06 +0530

    Compute the spiral ordering of a 2D array: Done

--- epi_judge_cpp/spiral_ordering_segments.cc
+++ epi_judge_cpp/spiral_ordering_segments.cc
@@ -1,9 +1,27 @@
 #include <vector>
+#include <cmath>
 #include "test_framework/generic_test.h"
 using std::vector;
+using namespace std;
+
 vector<int> MatrixInSpiralOrder(vector<vector<int>> square_matrix) {
-  // TODO - you fill in here.
-  return {};
+  int n = square_matrix.size();
+  int layers = ceil(n/2.0);
+
+  vector<int> res;
+  for(int l = 0; l < layers; l++){
+    int r = l, c = l;
+    do{
+      res.emplace_back(square_matrix[r][c]);
+      if(r == l && c < n-1-l) c++;
+      else if(r == n-1-l && c > l) c--;
+      else if(c == l && r > l)r--;
+      else if(c == n-1-l && r < n-1-l)r++;
+
+    }while(r != l || c != l);
+  }
+
+  return res;
 }
 
 int main(int argc, char* argv[]) {
