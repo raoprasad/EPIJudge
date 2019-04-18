61e77237ae1c19d4e27d58e9bf8a4a2bb9848e25
prasad <prasad.rao@gmail.com>
2019-04-18 10:27:26 +0530

    Compute binary tree nodes in order of increasing depth: Done:

--- epi_judge_cpp/tree_level_order.cc
+++ epi_judge_cpp/tree_level_order.cc
@@ -2,13 +2,31 @@
 #include <vector>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
-using std::unique_ptr;
-using std::vector;
+#include <queue>
+using namespace std;
 
 vector<vector<int>> BinaryTreeDepthOrder(
     const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+    vector<vector<int>> res;
+    if(tree == nullptr) return res;
+
+    queue<BinaryTreeNode<int>*> thislevel, nextlevel;
+    thislevel.push(tree.get());
+    vector<int> temp;
+    while(!thislevel.empty()){
+        BinaryTreeNode<int>* curr = move(thislevel.front()); thislevel.pop();
+        temp.emplace_back(curr->data);
+        if(curr->left != nullptr) nextlevel.push(curr->left.get());
+        if(curr->right != nullptr) nextlevel.push(curr->right.get());
+
+        if(thislevel.empty()){
+            res.emplace_back(temp);
+            temp.clear();
+            thislevel.swap(nextlevel);
+        }
+    }
+
+    return res;
 }
 
 int main(int argc, char* argv[]) {
