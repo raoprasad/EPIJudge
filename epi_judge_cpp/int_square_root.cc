2bc3ef807e8eff037652b74a0ebf78e41b15519d
prasad <prasad.rao@gmail.com>
2019-04-20 06:08:21 +0530

    Compute the integer square root: Done. One overflow bug resolved.

--- epi_judge_cpp/int_square_root.cc
+++ epi_judge_cpp/int_square_root.cc
@@ -1,8 +1,25 @@
 #include "test_framework/generic_test.h"
 
 int SquareRoot(int k) {
-  // TODO - you fill in here.
-  return 0;
+  if(k < 2) return k;
+  int begin = 1, end = k/2 + 1, sqr = 1;
+  long long curr = 1, prd = 1;
+
+  while(begin < end){
+      curr = begin + (end - begin)/2;
+      prd = curr*curr;
+      if(prd == k){
+          return curr;
+      }
+      else if(prd < k){
+          sqr = curr;
+          begin = curr + 1;
+      }
+      else{
+          end = curr;
+      }
+  }
+  return sqr;
 }
 
 int main(int argc, char* argv[]) {
