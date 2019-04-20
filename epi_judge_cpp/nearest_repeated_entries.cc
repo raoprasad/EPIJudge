f63fd06cde8b5e7e511ab47cee6e90bfa00e2241
prasad <prasad.rao@gmail.com>
2019-04-20 07:42:38 +0530

    Find the nearest repeated entries in an array: Done

--- epi_judge_cpp/nearest_repeated_entries.cc
+++ epi_judge_cpp/nearest_repeated_entries.cc
@@ -1,12 +1,22 @@
 #include <string>
 #include <vector>
 #include "test_framework/generic_test.h"
+#include <unordered_map>
 using std::string;
 using std::vector;
+using namespace std;
 
 int FindNearestRepetition(const vector<string>& paragraph) {
-  // TODO - you fill in here.
-  return 0;
+  int nearest = numeric_limits<int>::max();
+  unordered_map<string, int> lastPositions;
+  for(int i = 0; i < paragraph.size(); i++){
+      string candidate = paragraph[i];
+      if(lastPositions.find(candidate) != lastPositions.end()){
+          nearest = min(nearest, i - lastPositions[candidate]);
+      }
+      lastPositions[candidate] = i;
+  }
+  return (nearest == numeric_limits<int>::max()) ? -1 : nearest;
 }
 
 int main(int argc, char* argv[]) {
