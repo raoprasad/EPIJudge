fcdc6c7ac21b3ebbb2b637b67d2a42b05cbe6ad4
prasad <prasad.rao@gmail.com>
2019-04-28 20:01:37 +0530

    Find the k largest elements in a BST: Done in O(2n)

--- epi_judge_cpp/k_largest_values_in_bst.cc
+++ epi_judge_cpp/k_largest_values_in_bst.cc
@@ -5,9 +5,25 @@
 using std::unique_ptr;
 using std::vector;
 
+void findSize(BstNode<int>* tree, int& size){
+    if(tree->left != nullptr) findSize(tree->left.get(), size);
+    size++;
+    if(tree->right != nullptr) findSize(tree->right.get(), size);
+}
+void PopulateLastKNodes(BstNode<int>* tree, vector<int>& res, int size, int& curr, int k){
+    if(tree->left != nullptr) PopulateLastKNodes(tree->left.get(), res, size, curr, k);
+    curr++;
+    if(curr >= size - k + 1) res.emplace_back(tree->data);
+    if(tree->right != nullptr) PopulateLastKNodes(tree->right.get(), res, size, curr, k);
+}
 vector<int> FindKLargestInBST(const unique_ptr<BstNode<int>>& tree, int k) {
-  // TODO - you fill in here.
-  return {};
+    int size = 0, curr = 0;
+    findSize(tree.get(), size);
+
+    vector<int> res;
+
+    PopulateLastKNodes(tree.get(), res, size, curr, k);
+    return res;
 }
 
 int main(int argc, char* argv[]) {
